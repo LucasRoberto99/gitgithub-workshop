7c50d334c2753e5bab86b9e92fbc2f21f6dc0550
TomRoberto <tom.roberto31@gmail.com>
2026-06-18 11:59:07 +0200

    changed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: "orange" }}>
-      Coucou, je suis le footer de Tom !!!!!
+    <footer style={{ backgroundColor: "yellow" }}>
+      Coucou, je m'appelle Tom et j'ai créé un footer très cool.
     </footer>
   );
 };

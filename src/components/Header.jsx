e3776146db8c332379bd8fbb46ff5f18e871d4e5
Lucas Roberto <lucasr.prof@gmail.com>
2026-06-18 11:18:31 +0200

    header text change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header>
-      <p style={{ color: "white" }}>Header ici héhé</p>
+      <p style={{ color: "white" }}>Header ici master</p>
     </header>
   );
 };

b7b9dc28027944054fa1090f453c57a1895a673d
Lucas Roberto <lucasr.prof@gmail.com>
2026-06-18 11:15:45 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>Ceci est un projet</h1>
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <p style={{ color: "white" }}>Header ici héhé</p>
+    </header>
+  );
+};
+
+export default Header;
